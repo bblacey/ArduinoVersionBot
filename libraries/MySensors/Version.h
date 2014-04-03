a01b60deb5b20c5af1866bd417b301d5f6aa8049
Bruce Lacey <bruce@blacey.com>
2014-04-02 21:39:16 -0700

    Initial version file

--- libraries/MySensors/Version.h
+++ libraries/MySensors/Version.h
@@ -1,1 +1,11 @@
-#DEFINE VSN=""
+/***
+ *  This file defines the Sensor library version number 
+ *  Normally, contributors should not modify this directly
+ *  as it is manaaged by the MySensors build bot.
+ */
+#ifndef Sendor_h
+#define Sensor_h
+
+#define LIBRARY_VERSION "1.2+"
+
+#endif
