79730ebfc3ef6b05884ec25ced0c14fd5ae5aaaa
Bruce Lacey <bruce@blacey.com>
2014-04-03 18:52:50 -0700

    Version-Bot: updated version identifier to 1.1b2 (470d61d)

--- libraries/MySensors/Version.h
+++ libraries/MySensors/Version.h
@@ -3,9 +3,9 @@
  *  Normally, contributors should not modify this directly
  *  as it is manaaged by the MySensors build bot.
  */
-#ifndef Version_h
-#define Version_h
+#ifndef Sendor_h
+#define Sensor_h
 
-#define LIBRARY_VERSION "1.2+"
+#define LIBRARY_VERSION "1.1b2 (470d61d)"
 
 #endif
