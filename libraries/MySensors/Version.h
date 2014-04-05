2f618968d76bf28988d2968ff398042bf4e208da
Bruce Lacey <bruce@blacey.com>
2014-04-05 16:02:16 -0700

    Fixed #define typo in Version.h

--- libraries/MySensors/Version.h
+++ libraries/MySensors/Version.h
@@ -3,7 +3,7 @@
  *  Normally, contributors should not modify this directly
  *  as it is manaaged by the MySensors build bot.
  */
-#ifndef Sendor_h
+#ifndef Sensor_h
 #define Sensor_h
 
 #define LIBRARY_VERSION "1.1b2 (300f536)"
