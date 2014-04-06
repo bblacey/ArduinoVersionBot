9e8c2fdc3862574ff9f76574c65b0bd3ae856929
Bruce Lacey <bruce@blacey.com>
2014-04-05 22:58:21 -0700

    Bot test.

--- libraries/MySensors/Sensor.cpp
+++ libraries/MySensors/Sensor.cpp
@@ -4,7 +4,7 @@
 
  Created by Henrik Ekblad <henrik.ekblad@gmail.com>
 
- This program is free software; you can redistribute it and/or
+ This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License
  version 2 as published by the Free Software Foundation.
  */
