9d389d3f6bcedd6f9a808d386fdaac45c9d84c70
Bruce Lacey <bruce@blacey.com>
2014-04-06 11:18:05 -0700

    Another bot test.

--- libraries/MySensors/Gateway.cpp
+++ libraries/MySensors/Gateway.cpp
@@ -4,7 +4,6 @@
 
  Created by Henrik Ekblad <henrik.ekblad@gmail.com>
 
-
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  version 2 as published by the Free Software Foundation. 
